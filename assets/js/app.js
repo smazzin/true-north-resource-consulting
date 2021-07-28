35b7b67fd66d4fc8e812d472200ad1b69d2dd6af
Josh Hausotter <jhausotter@gmail.com>
2021-07-28 10:31:09 -0700

    add jquery for menu

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// This is where it all goes :)
+(function ($) {
+    'use strict';
+
+    $(document).ready(function () {
+        // Toggle menu on click
+        $("#menu-toggler").click(function () {
+            toggleBodyClass("menu-active");
+        });
+
+         function toggleBodyClass(className) {
+            document.body.classList.toggle(className);
+        }
+
+    });
+
+})(jQuery);
